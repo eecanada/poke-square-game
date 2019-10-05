a523536f404fb9e93f7a4e1608a5ac1d71a7a84b
Eder Canada <edercanada@Eders-MacBook-Pro.local>
2019-10-04 18:31:28 -0700

    random color

--- js/app.js
+++ js/app.js
@@ -11,7 +11,27 @@ const game = {
   createSquares(numberOfSquares){
     for (let i = 0; i < numberOfSquares; i++){
         const $square = $('<div/>');
+        
+        this.applyRandomColor($square)
+        
         $('.squares').append($square);
     }
+  },
+
+ 
+  applyRandomColor($square){
+    // 3 colors are assigned a digit 
+    const randomNUmber = Math.floor(Math.random() * 3)
+    if (randomNUmber === 0){
+       $square.css('background-color','red')
+    } else if (randomNUmber === 1 ){
+      $square.css('background-color','blue')
+    } else if (randomNUmber === 2){
+      $square.css('background-color','green')
+    }
+
+    
+
+
   }
 }
