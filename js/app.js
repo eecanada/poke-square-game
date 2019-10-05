083711c8b059b3e9c55a7bedf09246707d65bbb5
Eder Canada <edercanada@Eders-MacBook-Pro.local>
2019-10-04 18:10:45 -0700

    created squares

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 // make a button with an event listener 
-
 $('button').on('click', () => {
   console.log('button works')
 
+  // invoke function to populate squares
+  game.createSquares(50);
 });
+
+// make the game object to make the squares populate 
+const game = {
+  createSquares(numberOfSquares){
+    for (let i = 0; i < numberOfSquares; i++){
+        const $square = $('<div/>');
+        $('.squares').append($square);
+    }
+  }
+}
